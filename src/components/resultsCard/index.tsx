98e031289977ba643e9514e2514c1fd193c71a7c
adetayo abiodun <adetayo.abiodun@sterling.ng>
2022-04-04 13:44:00 +0100

    added animation

--- src/components/resultsCard/index.tsx
+++ src/components/resultsCard/index.tsx
@@ -7,15 +7,16 @@ type ResultCardProps = {
 
 const ResultsCard = ({ results }: ResultCardProps) => {
   return (
-    <div className="team">
-      <div className="team_member">
-        <div className="team_img mb-3">
-          <img src={results.avatar_url} alt="Team_image" />
+    <div className="profile">
+      <div className="profile_member">
+        <div className="profile_img mb-3">
+          <img src={results.avatar_url} alt="profile_image" />
         </div>
         <div className="hideText">
-          <h3>{results.login}</h3>
-          <p>{results.html_url.substr(0, 25) + '...'}</p>
-          <p>{`${results.score}.0`}</p>
+          <h3 className="">{results.login}</h3>
+          <p className="">{results.html_url}</p>
+          {/* <p className="">{results.html_url.substr(0, 25) + '...'}</p> */}
+          <p className="">{`${results.score}.0`}</p>
         </div>
       </div>
     </div>
